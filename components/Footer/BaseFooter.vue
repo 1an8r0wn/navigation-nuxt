99098388601f2137fa8e14bfa6d65a7cf2036917
Edward-Brock <1416477069@qq.com>
2024-01-26 20:49:49 +0800

    refactor: 将 Footer 中的主题切换移动到 Header 中；

--- components/Footer/BaseFooter.vue
+++ components/Footer/BaseFooter.vue
@@ -6,71 +6,12 @@ const nowYear = new Date().getFullYear()
 
 // 获取 package.json 当前版本号
 const version = config.version
-
-/**
- * ==================================================
- * 提供并展示当前颜色主题
- * ==================================================
- */
-
-const colorMode = useColorMode()
-const colorModeIcon = reactive({
-  index: 0,
-  value: '',
-  icon: '',
-})
-const colorModeList = [{
-  value: 'system',
-  icon: 'i-heroicons-tv',
-}, {
-  value: 'light',
-  icon: 'i-heroicons-sun',
-}, {
-  value: 'dark',
-  icon: 'i-heroicons-moon',
-}]
-
-/**
- * 切换颜色模式
- * 通过对 colorMode 内的 index 进行累加切换当前颜色模式
- * 当累加超过颜色模式列表长度时重置为 0 即为 “system（自动）”
- */
-function switchColorModeHandler() {
-  colorModeIcon.index++
-  // 判断 index 是否大于颜色模式列表长度，若大于则将 index 重置为 0 并根据 index 覆盖颜色模式名称及图标
-  if (colorModeIcon.index >= colorModeList.length) {
-    colorModeIcon.index = 0
-    colorModeIcon.value = colorModeList[colorModeIcon.index].value
-    colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
-  }
-  colorModeIcon.value = colorModeList[colorModeIcon.index].value
-  colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
-  colorMode.preference = colorModeList[colorModeIcon.index].value
-}
-
-onMounted(() => {
-  /**
-   * 页面初始化时通过 colorMode 模块获取当前系统缓存的颜色模式值
-   * 通过循环颜色模式列表 colorModeList 获取当前值所包含的名称及图标信息保存到 colorModeIcon 中
-   */
-  colorModeIcon.value = colorMode.preference
-  for (let i = 0; i < colorModeList.length; i++) {
-    if (colorModeIcon.value === colorModeList[i].value) {
-      colorModeIcon.index = i
-      colorModeIcon.value = colorModeList[colorModeIcon.index].value
-      colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
-    }
-  }
-})
 </script>
 
 <template>
   <footer
     class="flex flex-col h-full p-4 sm:p-9 border-t-2 border-zinc-100 bg-zinc-50 dark:border-zinc-900 dark:bg-zinc-950 select-none"
   >
-    <div class="flex items-baseline sm:items-center justify-between flex-col sm:flex-row">
-      <UButton :icon="colorModeIcon.icon" size="sm" color="black" square variant="link" @click="switchColorModeHandler()" />
-    </div>
     <div class="flex flex-col mt-4 text-xs text-zinc-300 dark:text-zinc-600">
       <div>VERSION: {{ version }}</div>
       <div>Copyright © {{ nowYear }} booop All rights reserved.</div>
--- components/Header/BaseHeader.vue
+++ components/Header/BaseHeader.vue
@@ -1,5 +1,61 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
+
+/**
+ * ==================================================
+ * 提供并展示当前颜色主题
+ * ==================================================
+ */
+
+const colorMode = useColorMode()
+const colorModeIcon = reactive({
+  index: 0,
+  value: '',
+  icon: '',
+})
+const colorModeList = [{
+  value: 'system',
+  icon: 'i-heroicons-tv',
+}, {
+  value: 'light',
+  icon: 'i-heroicons-sun',
+}, {
+  value: 'dark',
+  icon: 'i-heroicons-moon',
+}]
+
+/**
+ * 切换颜色模式
+ * 通过对 colorMode 内的 index 进行累加切换当前颜色模式
+ * 当累加超过颜色模式列表长度时重置为 0 即为 “system（自动）”
+ */
+function switchColorModeHandler() {
+  colorModeIcon.index++
+  // 判断 index 是否大于颜色模式列表长度，若大于则将 index 重置为 0 并根据 index 覆盖颜色模式名称及图标
+  if (colorModeIcon.index >= colorModeList.length) {
+    colorModeIcon.index = 0
+    colorModeIcon.value = colorModeList[colorModeIcon.index].value
+    colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
+  }
+  colorModeIcon.value = colorModeList[colorModeIcon.index].value
+  colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
+  colorMode.preference = colorModeList[colorModeIcon.index].value
+}
+
+onMounted(() => {
+  /**
+   * 页面初始化时通过 colorMode 模块获取当前系统缓存的颜色模式值
+   * 通过循环颜色模式列表 colorModeList 获取当前值所包含的名称及图标信息保存到 colorModeIcon 中
+   */
+  colorModeIcon.value = colorMode.preference
+  for (let i = 0; i < colorModeList.length; i++) {
+    if (colorModeIcon.value === colorModeList[i].value) {
+      colorModeIcon.index = i
+      colorModeIcon.value = colorModeList[colorModeIcon.index].value
+      colorModeIcon.icon = colorModeList[colorModeIcon.index].icon
+    }
+  }
+})
 </script>
 
 <template>
@@ -8,10 +64,13 @@ const appConfig = useAppConfig()
   >
     <div class="w-full flex items-center justify-between">
       <img class="h-9" src="assets/image/booop_navigation_logo_w500_h150_black.png" alt="booop navigation logo">
-      <UButton
-        icon="i-mdi-github" size="lg" color="black" variant="link" :label="appConfig.github.title" :trailing="false"
-        :to="appConfig.github.url" target="_blank"
-      />
+      <div>
+        <UButton :icon="colorModeIcon.icon" size="sm" color="black" square variant="link" @click="switchColorModeHandler()" />
+        <UButton
+          icon="i-mdi-github" size="lg" color="black" variant="link" :label="appConfig.github.title" :trailing="false"
+          :to="appConfig.github.url" target="_blank"
+        />
+      </div>
     </div>
   </header>
 </template>
