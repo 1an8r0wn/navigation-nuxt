0ae6c23127d2a95477320a263a059e64db5d4a4d
Edward-Brock <1416477069@qq.com>
2024-02-01 22:40:19 +0800

    fix: 临时取消 lodash 防抖函数导致的服务不可用问题； style: 优化页面显示效果；

--- components/Search/index.vue
+++ components/Search/index.vue
@@ -1,29 +1,23 @@
 <script setup lang="ts">
-import { debounce } from 'lodash'
 import { useNavigationStore } from '~/stores'
 
 const store = useNavigationStore()
 const inputValue = ref('')
 
-async function fetchData(keyword: string) {
-  if (keyword !== '') {
+watch(() => inputValue.value, async () => {
+  if (inputValue.value !== '') {
     // inputValue 值不为空则将 keyword 通过 get 方法传至 searchKeyword 接口
-    const categoryList = await $fetch('/api/category/searchKeyword', {
+    const { data: categoryList } = await useFetch('/api/category/searchKeyword', {
       method: 'get',
-      query: { keyword },
+      query: { keyword: inputValue.value },
     })
     store.updateNavigationList(categoryList)
   }
   else {
     // inputValue 值为空时，恢复调用 allNavigationListData 接口
-    const categoryList = await $fetch('/api/category/allNavigationListData')
+    const { data: categoryList } = await useFetch('/api/category/allNavigationListData')
     store.updateNavigationList(categoryList)
   }
-}
-
-const debouncedFetch = debounce((keyword: string) => fetchData(keyword), 300)
-watch(() => inputValue.value, (keyword) => {
-  debouncedFetch(keyword)
 })
 </script>
 
